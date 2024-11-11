c16a1cba5ef2f183b139b12ffe710dfd7cbc95e6
gertecdeveloper <dev@gertec.com.br>
2024-11-11 17:48:37 -0300

    Atualizacao de exemplos

--- Android_Nativo_Java/Projeto GS300 antigo/app/src/main/java/com/example/pdv_android_gs300/GlobalValues.java
+++
@@ -1,6 +0,0 @@
-package com.example.pdv_android_gs300;
-
-public class GlobalValues {
-    public static String codAtivacao = "";
-    public static String ultimaChaveVenda = "";
-}
--- Android_Nativo_Java/Projeto GS300 antigo/app/src/main/java/com/example/pdv_android_gs300/nfc/Interfaces/Listener.java
+++
@@ -1,9 +0,0 @@
-package com.example.pdv_android_gs300.nfc.Interfaces;
-
-public interface Listener {
-
-    void onDialogDisplayed();
-
-    void onDialogDismissed();
-
-}
